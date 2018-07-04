542e3f1fece1c1aefc23aad50ba50f99387abb05
Unknown <Mahekaru@yahoo.com>
2018-07-04 02:24:09 -0400

    Adding application to git

---
+++ TelephoneNumberWordGenerator/TelephoneNumberWordGenerator.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TelephoneNumberWordGenerator
+   Inherits System.Windows.Forms.Form
+
+   'Form overrides dispose to clean up the component list.
+   <System.Diagnostics.DebuggerNonUserCode()> _
+   Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+      Try
+         If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+         End If
+      Finally
+         MyBase.Dispose(disposing)
+      End Try
+   End Sub
+
+   'Required by the Windows Form Designer
+   Private components As System.ComponentModel.IContainer
+
+   'NOTE: The following procedure is required by the Windows Form Designer
+   'It can be modified using the Windows Form Designer.  
+   'Do not modify it using the code editor.
+   <System.Diagnostics.DebuggerStepThrough()> _
+   Private Sub InitializeComponent()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.btnGenerateWords = New System.Windows.Forms.Button()
+        Me.txtPhoneNumber = New System.Windows.Forms.TextBox()
+        Me.lblPhoneNumber = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Font = New System.Drawing.Font("Lucida Console", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOutput.Location = New System.Drawing.Point(12, 76)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtOutput.Size = New System.Drawing.Size(457, 357)
+        Me.txtOutput.TabIndex = 11
+        '
+        'btnGenerateWords
+        '
+        Me.btnGenerateWords.Location = New System.Drawing.Point(12, 41)
+        Me.btnGenerateWords.Name = "btnGenerateWords"
+        Me.btnGenerateWords.Size = New System.Drawing.Size(457, 29)
+        Me.btnGenerateWords.TabIndex = 10
+        Me.btnGenerateWords.Text = "Generate Words"
+        '
+        'txtPhoneNumber
+        '
+        Me.txtPhoneNumber.Location = New System.Drawing.Point(287, 12)
+        Me.txtPhoneNumber.MaxLength = 7
+        Me.txtPhoneNumber.Name = "txtPhoneNumber"
+        Me.txtPhoneNumber.Size = New System.Drawing.Size(182, 23)
+        Me.txtPhoneNumber.TabIndex = 9
+        '
+        'lblPhoneNumber
+        '
+        Me.lblPhoneNumber.Location = New System.Drawing.Point(15, 15)
+        Me.lblPhoneNumber.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblPhoneNumber.Name = "lblPhoneNumber"
+        Me.lblPhoneNumber.Size = New System.Drawing.Size(265, 20)
+        Me.lblPhoneNumber.TabIndex = 8
+        Me.lblPhoneNumber.Text = "Enter phone number (digits greater than 1 only):"
+        '
+        'TelephoneNumberWordGenerator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(482, 446)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.btnGenerateWords)
+        Me.Controls.Add(Me.txtPhoneNumber)
+        Me.Controls.Add(Me.lblPhoneNumber)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "TelephoneNumberWordGenerator"
+        Me.Text = "Telephone Number Word Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtOutput As System.Windows.Forms.TextBox
+    Friend WithEvents btnGenerateWords As System.Windows.Forms.Button
+    Friend WithEvents txtPhoneNumber As System.Windows.Forms.TextBox
+    Friend WithEvents lblPhoneNumber As System.Windows.Forms.Label
+
+End Class
---
+++ TelephoneNumberWordGenerator/TelephoneNumberWordGenerator.vb
@@ -0,0 +1,56 @@
+﻿Public Class TelephoneNumberWordGenerator
+    Private Sub generateWordsButton_Click(ByVal sender As System.Object,
+       ByVal e As System.EventArgs) Handles btnGenerateWords.Click
+
+        If Not IsNumeric(txtPhoneNumber.Text) Then
+            txtPhoneNumber.Text = ""
+        Else
+            If txtPhoneNumber.Text.Length < 7 Then
+                MessageBox.Show("Phone Number Must Be 7 Digits Long")
+            Else
+                Dim phoneNumber As String = txtPhoneNumber.Text
+
+                Dim letters(,) As String = {{" ", " ", " "},
+                    {" ", " ", " "}, {"A", "B", "C"}, {"D", "E", "F"},
+                   {"G", "H", "I"}, {"J", "K", "L"}, {"M", "N", "O"},
+                   {"P", "R", "S"}, {"T", "U", "V"}, {"W", "X", "Y"}}
+                Dim digits(6) As Integer
+
+                Dim output As String = String.Empty
+
+                For i = 0 To phoneNumber.Length - 1
+                    digits(i) = phoneNumber.Substring(i, 1)
+                Next
+
+                For loop1 = 0 To 2
+                    For loop2 = 0 To 2
+                        For loop3 = 0 To 2
+                            For loop4 = 0 To 2
+                                For loop5 = 0 To 2
+                                    For loop6 = 0 To 2
+                                        For loop7 = 0 To 2
+                                            output &= String.Format("{0, -10}",
+                                               letters(digits(0), loop1) &
+                                               letters(digits(1), loop2) &
+                                               letters(digits(2), loop3) &
+                                               letters(digits(3), loop4) &
+                                               letters(digits(4), loop5) &
+                                               letters(digits(5), loop6) &
+                                               letters(digits(6), loop7))
+                                        Next loop7
+                                    Next loop6
+                                Next loop5
+                            Next loop4
+                        Next loop3
+                    Next loop2
+                Next loop1
+
+                txtOutput.Text = output
+            End If
+        End If
+    End Sub
+
+    Private Sub TelephoneNumberWordGenerator_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
